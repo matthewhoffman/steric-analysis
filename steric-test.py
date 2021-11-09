c132cb122ddd518c9d127cb9af0fefaedc1dcea4
Matthew Hoffman <mhoffman@lanl.gov>
2021-11-08 20:04:23 -0800

    Do some clean up, comment a lot

--- steric-test.py
+++ steric-test.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 '''
 Try to calculate and plot steric sea level change from E3SM runs
+
+Notes see:
+Griffies, S.M., Yin, J., Durack, P.J., Goddard, P., Bates, S.C., Behrens, E., Bentsen, M., Bi, D., Biastoch, A., Böning, C.W., Bozec, A., Chassignet, E., Danabasoglu, G., Danilov, S., Domingues, C.M., Drange, H., Farneti, R., Fernandez, E., Greatbatch, R.J., Holland, D.M., Ilicak, M., Large, W.G., Lorbacher, K., Lu, J., Marsland, S.J., Mishra, A., George Nurser, A.J., Salas y Mélia, D., Palter, J.B., Samuels, B.L., Schröter, J., Schwarzkopf, F.U., Sidorenko, D., Treguier, A.M., Tseng, Y. heng, Tsujino, H., Uotila, P., Valcke, S., Voldoire, A., Wang, Q., Winton, M., Zhang, X., 2014. An assessment of global and regional sea level for years 1993-2007 in a suite of interannual core-II simulations. Ocean Model. 78, 35-89. doi:10.1016/j.ocemod.2014.03.004
+
+Griffies, S.M., Greatbatch, R.J., 2012. Physical processes that impact the evolution of global mean sea level in ocean climate models. Ocean Model. 51, 37-72. doi:10.1016/j.ocemod.2012.04.003
+
+
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals
@@ -35,21 +42,8 @@ pii=3.14159
 g=9.8101
 
 
-# ---- Choose time(s) -----
-#yrs=(10, 30, 50, 60, 70,80, 90, 100, 150)
-#yrs=(10, 30, 50, 60, 70,80, 90, 100, 100)
-#yrs=np.arange(1,150,1)
-yrs=np.arange(1,120,2)
-
-#yrs=(50, 50, 50, 60, 70,80, 90, 100, 150)
-#yrs = np.arange(95,102,1)
-mos=(1,)
-
-mo=1
-#mos=np.arange(1,13,1)
-# -------------------------
-
 # ---- Choose run directory ----
+# These contain E3SM v1 files downloaded manually from ESGF
 path='/project/projectdirs/m3412/simulations/20190819.GMPAS-DIB-IAF-ISMF.T62_oEC60to30v3wLI.cori-knl.testNewGM/archive/ocn/hist'
 path='/global/cscratch1/sd/hoffman2/4xc02'
 path='/global/cscratch1/sd/hoffman2/SLR_tests/lowres_hist_ens1'
@@ -58,21 +52,31 @@ path='/global/cscratch1/sd/hoffman2/SLR_tests/lowres_hist_ens1'
 
 
 # ---- Choose spatial extent -----
-#idx = np.nonzero(np.logical_and(np.logical_and(latCell<-70.0/180.0*pii, latCell>-85.0/180.0*pii), np.logical_and(lonCell>300.0/360.0*2.0*pii, lonCell<350.0/360.0*2*pii)))[0]  #entire weddell
-#idx = np.nonzero(np.logical_and(np.logical_and(latCell<-70.0/180.0*pii, latCell>-85.0/180.0*pii), np.logical_and(lonCell>270.0/360.0*2.0*pii, lonCell<350.0/360.0*2*pii)))[0]  #entire weddell wider
-#idx = np.nonzero( (latCell<-60.0/180.0*pii) * (latCell>-85.0/180.0*pii) * np.logical_or(lonCell>280.0/360.0*2.0*pii, lonCell<80.0/360.0*2*pii))[0]; size=1.4; fsz=(15,9)  # weddell to Amery 
-idx = np.nonzero( (latCell<-50.0/180.0*pii) )[0] # SO
-
-idx = np.nonzero(latCell<99999999999999999)[0]
+# Code below adopted from some regional analysis scripts, so there is option to subset the data.
+# But for this script would probably be less confusing to drop this.  Leaving for now.
+#idx = np.nonzero( (latCell<-50.0/180.0*pii) )[0] # SO
+idx = np.nonzero(latCell<99999999999999999)[0] # global ocean
 
 
 print("Found {} cells in idx".format(len(idx)))
 
 mo=1
+
+# Define rho_0 from e.g. Griffies et al. (2014) Eq. 54.  Results not sensitive to the choice because 
+# choices (and impact on results) only vary by ~1%.  Griffies uses 1035.  See Eq. 6.
 rho_ref = 1036.0 # typical surface density
 #rho_ref = 1026.0 # E3SM rho_sw
 
 def SL(file):
+    '''
+    Function to calculate sea level for a given file (time slice).
+    This is based on the equations in G14 Appendix B.
+    Specifically, this function is calculating the local steric tendency, 
+    the second term in Eq. 47 and elaborated on in section B.1.2.
+    Fundamentally, I am just solving Eq. 55 for two different time periods
+    and then taking the difference.
+    '''
+
     SL = np.zeros((len(idx),))
     rho =            file.variables['timeMonthly_avg_density']       [0, :, :]
     layerThickness = file.variables['timeMonthly_avg_layerThickness'][0, :, :]
@@ -81,6 +85,7 @@ def SL(file):
     cnt = 0
     upperOnly = False
     if upperOnly:
+      # This option only considers upper water column and avoids dealing with drift in deep ocean
       # This way is much slower to run
       for i in idx:
         maxLev = maxLevelCell[i]
@@ -99,12 +104,17 @@ def SL(file):
             SL[cnt] += -1.0 / rho_ref * (rho[i, k+1] * (zLayerBot[k+1] - -700.0))
         cnt += 1
     else:
+      # This way considers entire water column. This should be ok if model output is drift-corrected.
       for i in idx:
 
         maxLev = maxLevelCell[i]
         #SL[cnt] = -1.0 / rho_ref * (rho[i, :maxLev] * layerThickness[i, :maxLev]).sum()
+        # Calculate SL as column height above the bottom depth.  Question: Does this differ from G14 Eq. 55
+        # which instead subtracts the steric height from the full sea level?  But where do they get 
+        # the "full sea level"?  Should that be 'ssh'?
         SL[cnt] = -1.0 / rho_ref * (rho[i, :maxLev] * layerThickness[i, :maxLev]).sum() + bottomDepth[i]
 
+        # Check for unphysical values
         if SL[cnt] > 990:
             print('maxLev=',maxLev,' bottomDepth=',bottomDepth[i], ' nz=', rho.shape, ' SL=',SL[cnt], ' ssh=',ssh[i], ' ssh_adj=',pressAdjSSH[i])
             print('rho:', rho[i,:])
@@ -112,6 +122,9 @@ def SL(file):
             print('layerThick:', layerThickness[i,:])
             sys.exit()
 
+        # Here complete G14 Eq. 55 by subtracting the steric height from SSH.
+        # The pressAdjSSH term is supposed to account for sea ice, but I'm not sure it works correctly.
+        # Outside of polar regions this should be right?  This doesn't look right, actually.
         SL[i] = SL[i] + (pressAdjSSH[i] - ssh[i]) * 1035.0/1026.0
         cnt += 1
 
@@ -121,16 +134,25 @@ def SL(file):
     return SL
 
 
+# --- Drift correction ---
+# In this section, estimate PI drift by taking the difference of two ten year averages.
+# The ten year averages were calculated with NCO manually from ten years of monthly averages.
+# In papers I've looked at, they don't explain exactly how they do this drift correction.
+# Should we be fitting a line to the whole PI time series?  Should we be averaging over more than 10 years?
+
 pipath='/global/cscratch1/sd/hoffman2/SLR_tests/piControl'
-yr_pi1=405
+yr_pi1=405 #nominal yr
 #fpi1=netCDF4.Dataset('{0}/mpaso.hist.am.timeSeriesStatsMonthly.{1:04d}-{2:02d}-01.nc'.format(pipath, yr_pi1, mo), 'r')
 fpi1=netCDF4.Dataset('/global/cscratch1/sd/hoffman2/SLR_tests/piControl/0400-0410/mpaso.hist.0400-0410.nc', 'r')
-yr_pi2=495
+yr_pi2=495 # nominal yr
 #fpi2=netCDF4.Dataset('{0}/mpaso.hist.am.timeSeriesStatsMonthly.{1:04d}-{2:02d}-01.nc'.format(pipath, yr_pi2, mo), 'r')
 fpi2=netCDF4.Dataset('/global/cscratch1/sd/hoffman2/SLR_tests/piControl/0490-0500/mpaso.hist.0490-0500.nc', 'r')
 drift = (SL(fpi2)-SL(fpi1))/(yr_pi2-yr_pi1) # m/yr
 
-# historical
+
+
+# --- Historical SLC ---
+# Also using ten year averages calculated offline by NCO.
 ens='ens2'
 yr= 2000
 #f=netCDF4.Dataset('{0}/mpaso.hist.am.timeSeriesStatsMonthly.{1:04d}-{2:02d}-01.nc'.format(path, yr, mo), 'r')
@@ -141,9 +163,16 @@ fref=netCDF4.Dataset('/global/cscratch1/sd/hoffman2/SLR_tests/lowres_hist_ens/en
 SLref = SL(fref)
 SLi = SL(f)
 
+# Calculate difference and rate
 SLCdiff = (SLi - SLref)
 SLCrate = (SLi - SLref) / (yr - yr_ref)
 
+
+# --- Plots ---
+
+# Plot the steric sea level height (not the change).
+# Locally sea level varies by 10s of meters from 0!
+# Strong imprinting of continental shelf / bottom depth.
 figSL = plt.figure(10, facecolor='w', figsize=(14, 6))
 nrow=1
 ncol=2
@@ -160,6 +189,9 @@ plt.colorbar()
 plt.title('2000 sea level (m)')
 
 
+# Plot SLC in PI control and historical.
+# Things are obviously wrong where there is sea ice.
+# But also clearly there is bathymetry imprinting as seen in previous figure.
 fig1 = plt.figure(1, facecolor='w', figsize=(14, 6))
 nrow=2
 ncol=2
@@ -197,8 +229,13 @@ print("drift-corrected SLC mean={}".format(((SLCrate-drift)*areaCell).mean()/are
 #plt.colorbar()
 plt.draw()
 #plt.savefig('foo.png')
-   
-   
+
+
+
+# Plot SSH and SSH change.
+# I believe this represents dynamic sea level.
+# This has nothing to do with steric, but is the other ocean component we want.
+# This looks reasonable compared to previous papers.
 fig2 = plt.figure(2, facecolor='w', figsize=(14, 6))
 nrow=2
 ncol=2
